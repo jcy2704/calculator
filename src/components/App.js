08b6590b3e87be1000d81642fbce060ad3e584e0
Steven Jack Chung <stevenjchung12@gmail.com>
2021-02-25 11:35:01 -0500

    Fix linter error

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import '../App.css';
+// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
