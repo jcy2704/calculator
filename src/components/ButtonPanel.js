61902a90591b71bf70781656013d123cebb44f9e
Steven Jack Chung <stevenjchung12@gmail.com>
2021-03-01 13:42:11 -0500

    Add handleclick function to ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-const ButtonPanel = () => {
+const ButtonPanel = ({ clickHandler }) => {
+  const handleClick = buttonName => {
+    clickHandler(buttonName);
+  };
+
   const group = [['AC', '+/-', '%', '÷'], [7, 8, 9, 'X'], [4, 5, 6, '-'], [1, 2, 3, '+'], [0, '.', '=']];
 
   return (
@@ -9,12 +14,16 @@ const ButtonPanel = () => {
       {group.map(elem => (
         <div key={elem.id}>
           {elem.map(btn => (
-            <Button key={btn.id} name={btn} />
+            <Button key={btn.id} name={btn} clickHandler={handleClick} />
           ))}
         </div>
       ))}
     </>
   );
+};
+
+ButtonPanel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default ButtonPanel;
